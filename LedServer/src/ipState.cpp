256d982de439dc794a30ac4f69a34ecff44bb0cf
Kasper Rasmussen <kasper@obscura.dk>
2013-08-01 14:32:12 +0200

    Interface experiment
    Added local IPv4 address to the interface
    
    Signed-off-by: Kasper Rasmussen <kasper@obscura.dk>

---
+++ LedServer/src/ipState.cpp
@@ -0,0 +1,9 @@
+//
+//  ipState.cpp
+//  LedServer
+//
+//  Created by obscura on 01/08/13.
+//
+//
+
+#include "ipState.h"
---
+++ LedServer/src/ipState.h
@@ -0,0 +1,14 @@
+//
+//  ipState.h
+//  LedServer
+//
+//  Created by obscura on 01/08/13.
+//
+//
+
+#ifndef __LedServer__ipState__
+#define __LedServer__ipState__
+
+#include <iostream>
+
+#endif /* defined(__LedServer__ipState__) */
